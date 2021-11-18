02b3942599d6f718eed9d5b1aad492d5bf75c20a
Andrii <bezkrovnyyav@gmail.com>
2021-11-18 17:46:24 +0200

    Task_3 is fixed

--- HW_1/Task_3.py
+++ HW_1/Task_3.py
@@ -2,7 +2,7 @@
 
 total_sum = 0
 number = int(input("Enter the integers for summing: "))
-for element in range(number +1):
+for element in range(number + 1):
     total_sum += element
 
 print(total_sum)
