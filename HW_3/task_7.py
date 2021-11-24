c59fc41a46bd47f5740b06d4eeb3406768ffb69d
Andrii <bezkrovnyyav@gmail.com>
2021-11-24 19:39:39 +0200

    HW_3 is done

--- HW_3/task_7.py
+++ HW_3/task_7.py
@@ -1,2 +1,30 @@
 # 7. Ну і традиційно -> калькулятор :) повинна бути 1 ф-цiя яка б приймала 3 аргументи - один з яких операцiя, яку зробити!
 
+def calculator(first_number, action, second_number):
+    result = None
+
+    if action == '+':
+        result = first_number + second_number
+    elif action == '-':
+        result = first_number - second_number
+    elif action == '*':
+        result = first_number * second_number
+    elif action == '/':
+        if second_number == 0:
+            result = 'Division by zero is not possible!'
+        else:
+            result = first_number / second_number  
+
+    return result
+
+def get_numbers(number):
+    return int(input(f'Enter the {number}: '))
+
+if __name__ == '__main__':
+    operation = calculator(
+                           get_numbers('first number'),
+                           input('Input action [+, -, *, /]: '),
+                           get_numbers('second number')
+                           )
+                           
+    print(f'Result of operation is: {operation}')
