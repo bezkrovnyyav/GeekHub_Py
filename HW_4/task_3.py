85f84824aa42c24b1abd1156db4c5a84d774342c
Andrii <bezkrovnyyav@gmail.com>
2021-11-27 20:33:05 +0200

    Task_3 is made

--- HW_4/task_3.py
+++ HW_4/task_3.py
@@ -1,9 +1,12 @@
 # 3. Написати функцию < is_prime >, яка прийматиме 1 аргумент - число від 0 до 1000, 
 # і яка вертатиме True, якщо це число просте, и False - якщо ні.
 
-def is_prime(random_num):
+def is_prime():
+    random_num = int(input('Input random integer number from zero to 1000: '))
+    if random_num > 1000 or random_num < 0:
+        is_prime()
+
     return random_num > 1 and all(random_num % item for item in range(2, random_num))
 
 if __name__ == '__main__':
-    random_num = int(input('Input random integer number from zero to 1000: '))
-    print(is_prime(random_num))
+    print(is_prime())
