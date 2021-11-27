1a8ffcea87758a04a796409a6200d5c055704ee3
Andrii <bezkrovnyyav@gmail.com>
2021-11-27 17:54:19 +0200

    Task_8 is made

---
+++ HW_4/task_8.py
@@ -0,0 +1,24 @@
+'''
+8. Написати функцію, яка буде реалізувати логіку циклічного зсуву елементів в списку. 
+Тобто, функція приймає два аргументи: список і величину зсуву 
+(якщо ця величина додатня - пересуваємо з кінця на початок, 
+якщо від'ємна - навпаки - пересуваємо елементи з початку списку в його кінець).
+   Наприклад:
+       fnc([1, 2, 3, 4, 5], shift=1) --> [5, 1, 2, 3, 4]
+       fnc([1, 2, 3, 4, 5], shift=-2) --> [3, 4, 5, 1, 2]
+'''
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+shift_number = int(input("Input number for shift: "))
+
+def func_shift (numbers, shift_number):
+    if shift_number > 0:
+        for item in range(shift_number):
+            numbers.insert(0, numbers.pop())
+        return numbers
+    else :
+        for item in range(-shift_number):
+            numbers.append(numbers.pop(0))
+        return numbers
+
+if __name__ == '__main__':
+    print(func_shift(numbers, shift_number))
